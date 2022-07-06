71331b991f36b25be99e686237b3d658c253da00
lotco <kgsin7@gmail.com>
2022-07-06 19:49:39 +0900

    딥링크 모바일 추가

--- meta_login/src/App.js
+++ meta_login/src/App.js
@@ -3,6 +3,8 @@ import { useState } from "react";
 
 import "./App.css";
 import ConnectWalletButton from "./components/ConnectWalletButton";
+import mobileCheck from "./helpers/mobileCheck";
+import getLinker from "./helpers/deepLink";
 
 const App = () => {
   const [loading, setLoading] = useState(false);
@@ -12,6 +14,10 @@ const App = () => {
     setLoading(true);
 
     try {
+      const yourWebUrl = "mysite.com"; // Replace with your website domain
+      const deepLink = `https://metamask.app.link/dapp/${yourWebUrl}`;
+      const downloadMetamaskUrl = "https://metamask.io/download.html";
+
       if (window?.ethereum?.isMetaMask) {
         // Desktop browser
         const accounts = await window.ethereum.request({
@@ -20,6 +26,12 @@ const App = () => {
 
         const account = Web3.utils.toChecksumAddress(accounts[0]);
         setAddress(account);
+      } else if (mobileCheck()) {
+        // Mobile browser
+        const linker = getLinker(downloadMetamaskUrl);
+        linker.openURL(deepLink);
+      } else {
+        window.open(downloadMetamaskUrl);
       }
     } catch (error) {
       console.log(error);
