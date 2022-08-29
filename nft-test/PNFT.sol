e638a325e3082805c99d2c9f5f42aead7e877ddf
Remix Workspace <>
2022-08-29 17:20:43 +0900

    생성자 작업 완료

--- nft-test/PNFT.sol
+++ nft-test/PNFT.sol
@@ -8,36 +8,35 @@ import "./contracts/token/ERC721/IERC721.sol";
 import "./contracts/access/Ownable.sol";
 import "./contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract PNFT is Ownable, ERC721URIStorage {      
+
+interface IPNFT721{
+    function mint(address recipient, uint256 tokenId, string memory tokenURI) external;
+}
+
+contract PNFT is Ownable, ERC721URIStorage, IPNFT721 {      
     
     constructor() ERC721("yayahoho", "YAHO") {
     } 
-    function mint(address recipient, uint256 tokenId, string memory tokenURI) public onlyOwner {        
+    function mint(address recipient, uint256 tokenId, string memory tokenURI) public override{
         _mint(recipient, tokenId);
         _setTokenURI(tokenId, tokenURI);        
     }
 }
 
-
-contract pProjectController {
-    IERC721 pnft;
+contract pProjectController{
+    IPNFT721 pnft;
     address _nftContractAddress;
-// 0xf8e81D47203A594245E36C48e151709F0C19fBe8
+
     constructor(address nftContractAddress){
         _nftContractAddress = nftContractAddress;
-        pnft = IERC721(nftContractAddress);
+        pnft = IPNFT721(nftContractAddress);
     }
 
-    function nftCount(uint256 tokenId) public view returns(address){
-        return pnft.ownerOf(tokenId);
-    }
-        // function balanceOf(address owner) external view returns (uint256 balance);
-
-    function bbb(address check) public view returns(uint256){
-        return pnft.balanceOf(check);
+    function generateProjectNFT(address recipient, uint256 nftId, string memory tokenURI) public {
+        pnft.mint(recipient, nftId, tokenURI);
     }
     function test() public view returns(uint256){
-        return 10;
+        return 11;
     }
     
 }
